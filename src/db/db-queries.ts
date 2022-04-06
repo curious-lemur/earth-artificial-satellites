de0c5fb635dd39264da62edf3beabda24f2d4c09
composercoder <valentinachernova1402@gmail.com>
2022-04-07 02:16:46 +0300

    add fake data to db-queries.ts file

---
+++ src/db/db-queries.ts
@@ -0,0 +1,31 @@
+const USSR = {
+  name: "Советский Союз",
+  satellitesList: [
+    {
+      name: "Спутник 1",
+      startupDate:  "14.04.1999"
+    },
+    {
+      name: "Спутник 2",
+      startupDate: "27.10.2017"
+    }
+  ]
+};
+
+const China = {
+  name: "Китай",
+  satellitesList: [
+    {
+      name: "zho feng xiao 1",
+      startupDate:  "14.04.1999"
+    },
+    {
+      name: "li wu mei 2",
+      startupDate: "27.10.2017"
+    }
+  ]
+};
+
+const data = [USSR, China];
+
+module.exports = data;
