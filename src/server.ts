072bcc2d8d511a157ffbf3514e389bfdd0fe584a
composercoder <valentinachernova1402@gmail.com>
2022-04-06 17:00:14 +0300

    update config import

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,20 @@
-const Config = require('./config/config');
+const config = require('./config/config');
+const express = require('express');
+const { graphqlHTTP } = require('express-graphql');
+const { Schema } = require('./schema/schema');
 
-console.log(Config);
+const root = {
+  hello: () => {
+    return 'Hello World'
+  }
+};
+
+const app = express();
+app.use('/graphql', graphqlHTTP({
+  schema: schema,
+  rootValue: root,
+  graphiql: true
+}));
+
+app.listen(config.PORT);
+console.log('Running a GraphQL API server at http://localhost:' + config.PORT + '/graphql');
