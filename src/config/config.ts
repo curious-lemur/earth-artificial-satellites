ecc3cdc72bb416c57ee25ea33834cecbb7b057d5
composercoder <valentinachernova1402@gmail.com>
2022-04-11 00:54:54 +0300

    move database connection string to config

--- src/config/config.ts
+++ src/config/config.ts
@@ -2,19 +2,23 @@ import { config } from 'dotenv';
 const env = config().parsed;
 
 type ConfigType = {
-  address: String,
-  port: String,
-  database: String,
-  dbUsername: String,
-  dbPassword: String
+  address: string,
+  port: string,
+  database: string,
+  dbUsername: string,
+  dbPassword: string,
+  connectionUrl: string
 }
 
 const Config: ConfigType = {
   address: env.ADDRESS || '127.0.0.1',
-  port: env.PORT || '3000',
-  database: env.DATABASE || 'earth_artificial_satellites',
+  port: env.PORT || '5984',
+  database: env.DATABASE || 'earth-artificial-satellites',
   dbUsername: env.DB_USERNAME || 'couchdb',
-  dbPassword: env.DB_PASSWORD || ''
+  dbPassword: env.DB_PASSWORD || '',
+  get connectionUrl(): string {
+    return `http://${this.dbUsername}:${this.dbPassword}@${this.address}:${this.port}/${this.database}`;
+  }
 }
 
 export default Config;
