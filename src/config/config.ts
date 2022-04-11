7ec08b24f1d3ba1b62277d0a6130be581b7ff7cc
composercoder <valentinachernova1402@gmail.com>
2022-04-11 22:24:40 +0300

    add apiPort field to config

--- src/config/config.ts
+++ src/config/config.ts
@@ -3,19 +3,21 @@ const env = config().parsed;
 
 type ConfigType = {
   address: string,
-  port: string,
-  database: string,
+  apiPort: string,
+  dbPort: string,
   dbUsername: string,
   dbPassword: string,
+  database: string,
   connectionUrl: string
 }
 
 const Config: ConfigType = {
   address: env.ADDRESS || '127.0.0.1',
-  port: env.PORT || '5984',
-  database: env.DATABASE || 'earth-artificial-satellites',
+  apiPort: env.API_PORT || '3000',
+  dbPort: env.DB_PORT || '5984',
   dbUsername: env.DB_USERNAME || 'couchdb',
   dbPassword: env.DB_PASSWORD || '',
+  database: env.DATABASE || 'earth-artificial-satellites',
   get connectionUrl(): string {
     return `http://${this.dbUsername}:${this.dbPassword}@${this.address}:${this.port}/${this.database}`;
   }
