82c788f7d2eeaa543e22434b3169cd58f9e80acb
composercoder <valentinachernova1402@gmail.com>
2022-04-27 23:26:39 +0300

    make a resolver for querying view function that returns satellite list

--- src/resolvers.ts
+++ src/resolvers.ts
@@ -1,11 +1,17 @@
 import Nano from 'nano';
 import Config from './config.js';
+import PaginationParams from './middleware/PaginationParams.js';
 
 const nano: Nano.ServerScope = <Nano.ServerScope>Nano(Config.connectionUrl);
 const db = nano.use(Config.database);
 
-async function findSatellites() {
-  const result = await db.view('satellites', 'all-satellites', { include_docs: true, limit: 5 });
+async function findSatellites({turnPageParams}) {
+  PaginationParams.update(turnPageParams);
+  const newParams = PaginationParams.getParams();
+  const result = await db.view('satellites', 'all-satellites', { include_docs: true, ...newParams});
+
+  PaginationParams.newTotalRows = result.total_rows;
+
   return result.rows || result || new Error('Docs not found');
 }
 
