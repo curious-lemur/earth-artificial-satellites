aa781f2439a2aa66523f22271379c56380f63313
composercoder <valentinachernova1402@gmail.com>
2022-05-10 00:18:21 +0300

    add satellite-list view, for querying satellites without countries, rename previous view,and emit country name from satellite-list view

--- design/views.js
+++ design/views.js
@@ -3,7 +3,19 @@
 
 function countryList(doc) {
   if (doc.docType === 'country') {
-    emit(doc._id, null);
+    emit(null, null);
+  }
+}
+
+// map function, json name: satellite-list-with-countries
+// its design document - satellites
+
+function satelliteListWithCountries(doc) {
+  if (doc.docType === "satellite") {
+    emit(null, null);
+    for (let i = 0; i < doc.countries.length; i++) {
+      emit(null, { "_id": doc.countries[i] })
+    }
   }
 }
 
@@ -12,9 +24,6 @@ function countryList(doc) {
 
 function satelliteList(doc) {
   if (doc.docType === "satellite") {
-    emit(null, null);
-    for (let i = 0; i < doc.countries.length; i++) {
-      emit(null, { "_id": doc.countries[i] })
-    }
+    emit(doc.country, null);
   }
 }
