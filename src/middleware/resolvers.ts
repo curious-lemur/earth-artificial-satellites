2d77dab7683e22808b3609c87ba632c7054ab96e
composercoder <valentinachernova1402@gmail.com>
2022-05-13 15:22:32 +0300

    add two new resolvers for querying satellite by id, and country by id

--- src/middleware/resolvers.ts
+++ src/middleware/resolvers.ts
@@ -1,10 +1,13 @@
-/// < reference path="../types/paging.ts" />
 import findSatellites from './resolvers/findSatellites.js';
 import findCountries from './resolvers/findCountries.js';
+import findOneSatellite from './resolvers/findOneSatellite.js';
+import findOneCountry from './resolvers/findOneCountry.js';
 
 const resolvers = {
   findSatellites,
-  findCountries
+  findCountries,
+  findOneSatellite,
+  findOneCountry
 };
 
 export default resolvers;
