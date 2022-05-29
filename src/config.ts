1e0e37165a91bc6e13c3aae242e576d6ae3dbdf9
composercoder <valentinachernova1402@gmail.com>
2022-05-30 01:09:31 +0300

    finish config

--- src/config.ts
+++ src/config.ts
@@ -1,26 +1,35 @@
 import { config } from 'dotenv';
 const env = config().parsed;
 
-type ConfigType = {
-  address: string,
-  apiPort: string,
-  dbPort: string,
-  dbUsername: string,
-  dbPassword: string,
-  database: string,
-  connectionUrl: string
-}
+class Config {
+  dbLocation: string
+  apiPort: string
+  dbPort?: string
+  dbAddress?: string
+  dbName: string
+  dbUsername: string
+  dbPassword: string
+  dbConnectionUrl: string
 
-const Config: ConfigType = {
-  address: env.ADDRESS || '127.0.0.1',
-  apiPort: env.API_PORT || '3000',
-  dbPort: env.DB_PORT || '5984',
-  dbUsername: env.DB_USERNAME || 'couchdb',
-  dbPassword: env.DB_PASSWORD || '',
-  database: env.DATABASE || 'earth-artificial-satellites',
-  get connectionUrl(): string {
-    return `http://${this.dbUsername}:${this.dbPassword}@${this.address}:${this.dbPort}`;
+  constructor() {
+    this.dbLocation = env.DB_LOCATION || 'atlas';
+    this.apiPort = env.API_PORT || '3000';
+    this.dbName = env.DBNAME || 'satellites-and-countries';
+    this.dbUsername = env.DB_USERNAME || 'admin';
+    this.dbPassword = env.DB_PASSWORD || 'admin';
+
+    switch (this.dbLocation) {
+      case 'atlas':
+        this.dbConnectionUrl = `mongodb+srv://${this.dbUsername}:${this.dbPassword}@cluster0.mvxew.mongodb.net/${this.dbName}?retryWrites=true&w=majority`;
+      case 'local':
+        this.dbPort = env.DB_PORT || '5000';
+        this.dbAddress = env.ADDRESS || '127.0.0.1';
+        this.dbConnectionUrl =  `${this.dbAddress}:${this.dbPort}/${this.dbName}`;
+      default:
+        console.error(new Error('Unknown database location'));
+    }
   }
+  
 }
 
-export default Config;
+export default new Config;
