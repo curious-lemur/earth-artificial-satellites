f7d4166ca3eb462eb42e6c30a04f23fb731b159a
composercoder <valentinachernova1402@gmail.com>
2022-06-07 23:41:18 +0300

    implement database initialization logic

--- src/db/db-init.ts
+++ src/db/db-init.ts
@@ -1,3 +1,49 @@
-import connect from './db-connection.js';
+import { readFile } from 'fs';
+import { promisify } from 'util';
+import { client, connect } from './db-connection.js';
 
-connect();
+class dbInitializer {
+    static async init(): Promise<void> {
+        const db = await connect();
+
+        const collections = ['countries', 'satellites'];
+        const collectionPromises = collections.map(async (collectionName) => {
+            const [error, data] = await this.readDataFromFile(collectionName);
+
+            if (error) {
+                console.error(error);
+                return 'not ok';
+            } else {
+                return await this.insertDataIntoDb(db, collectionName, JSON.parse(data));
+            }
+        });
+
+        await Promise.all(collectionPromises);
+
+        client.close(() => { console.log('Connection to database closed') });
+    }
+
+    static async readDataFromFile(collectionName): Promise<[Error, string]> {
+        const readFileData = promisify(readFile);
+        const data = await readFileData('./data/' + collectionName + '.json', 'utf8');
+        if (!data) {
+            return [new Error('Error while reading ' + collectionName + '.json file. Data not found.'), null];
+        } else {
+            return [null, data]
+        }
+    }
+
+    static async insertDataIntoDb(db, collectionName, data): Promise<string> {
+        try {
+            const collection = db.collection(collectionName);
+            const insertResult = await collection.insertMany(data.docs);
+            console.log('Successfully inserted documents into ' + collectionName + ' collection');
+            return 'ok';
+        } catch(err) {
+            console.error(err);
+            return 'not ok';
+        }
+    }
+}
+
+dbInitializer.init();
