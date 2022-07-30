0e7d33d4d2871fd09de97ca58685934b3dfb4a46
Valentina <valentinachernova1402@gmail.com>
2022-07-30 14:38:30 +0300

    implement error handling in config

--- src/config.ts
+++ src/config.ts
@@ -1,36 +1,49 @@
-import { config } from 'dotenv';
+import { config, DotenvParseOutput } from 'dotenv';
 const env = config().parsed;
 
 class Config {
-  dbLocation: string
+  dbLocation: string 
   apiPort: string
-  dbPort?: string
-  dbAddress?: string
   dbName: string
   dbUsername: string
   dbPassword: string
+  dbAddress: string
+  dbPort: string
+
   dbConnectionUrl: string
 
   constructor() {
-    this.dbLocation = env.DB_LOCATION || 'atlas';
-    this.apiPort = env.API_PORT || '3000';
-    this.dbName = env.DBNAME || 'satellites-and-countries';
-    this.dbUsername = env.DB_USERNAME || 'admin';
-    this.dbPassword = env.DB_PASSWORD || 'admin';
+
+    if (!env) { throw new Error('.env file was not found') }
+    this.dbLocation = this.getEnvVariable('DB_LOCATION', env);
+    this.apiPort = this.getEnvVariable('API_PORT', env);
+    this.dbName = this.getEnvVariable('DB_NAME', env);
+    this.dbUsername = this.getEnvVariable('DB_USERNAME', env);
+    this.dbPassword = this.getEnvVariable('DB_PASSWORD', env);
 
     switch (this.dbLocation) {
       case 'atlas':
         this.dbConnectionUrl = `mongodb+srv://${this.dbUsername}:${this.dbPassword}@cluster0.mvxew.mongodb.net/${this.dbName}?retryWrites=true&w=majority`;
         break;
       case 'local':
-        this.dbPort = env.DB_PORT || '5000';
-        this.dbAddress = env.ADDRESS || '127.0.0.1';
+        this.dbAddress = this.getEnvVariable('DB_ADDRESS', env);
+        this.dbPort = this.getEnvVariable('DB_PORT', env);
         this.dbConnectionUrl = `${this.dbAddress}:${this.dbPort}/${this.dbName}`;
         break;
       default:
-        console.error(new Error('Unknown database location'));
+        throw new Error('Unknown database location');
     }
-  } 
+  }
+  
+  getEnvVariable(varName: string, envFile: DotenvParseOutput): string { 
+    const varValue = envFile[varName];
+
+    if (!varValue) {
+      throw new Error(`.env variable ${varName} does not exist or is not valid`);
+    } else {
+      return varValue;
+    }
+  };
 }
 
 export default new Config;
