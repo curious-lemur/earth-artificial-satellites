4fe62f35dfe174e6f9610b8de1cc5e1933bd27dc
Valentina <valentinachernova1402@gmail.com>
2022-08-04 22:59:51 +0300

    correct errors in DbInitializer

--- src/db/DbInitializer.ts
+++ src/db/DbInitializer.ts
@@ -1,11 +1,6 @@
 import { readFile } from 'fs/promises';
 import { connect, close } from '../db/db-connection.js';
 
-type readFileResult = [
-    error: Error | null,
-    data: string | null
-]
-
 export default class DbInitializer {
     static async init(): Promise<void> {
         const db = await connect();
@@ -13,25 +8,24 @@ export default class DbInitializer {
 
         const collections = ['countries', 'satellites'];
         const collectionPromises = collections.map(async (collectionName) => {
-        readFile('./data/' + collectionName + '.json', 'utf8')
-            .catch(() => { 
-                console.error(new Error(`Unable to read ${collectionName}.json file`));
-             })
-            .then((data) => {
-                if (!data) throw new Error(`Data was not found in ${collectionName}.json file`);
-                this.insertDataIntoDb(db, collectionName, JSON.parse(data))
-            })
-            .catch((err) => console.error(err))
+            return readFile('./data/' + collectionName + '.json', 'utf8')
+                .catch(() => { 
+                    console.error(new Error(`Unable to read ${collectionName}.json file`));
+                })
+                .then(async (data) => {
+                    if (!data) throw new Error(`Data was not found in ${collectionName}.json file`);
+                    await this.insertDataIntoDb(db, collectionName, JSON.parse(data))
+                })
+                .catch((err) => console.error(err))
         });
 
         await Promise.all(collectionPromises);
-
         close();
     }
 
     static async insertDataIntoDb(db, collectionName, data): Promise<boolean> {
         try {
-            const collection = db.collection(collectionName);
+            const collection = await db.collection(collectionName);
             const insertResult = await collection.insertMany(data.docs);
             console.log('Successfully inserted documents into ' + collectionName + ' collection');
             return true;
